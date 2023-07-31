98105005838e11f066296e0ec5263456b9f3b269
Vitória <101210358+Ranci777@users.noreply.github.com>
2023-07-31 17:15:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function addToCart(productName, price) {
+  // Aqui você pode implementar a lógica para adicionar o produto ao carrinho de compras.
+  // Por exemplo, você pode usar uma API para enviar os dados para um servidor ou apenas
+  // exibir uma mensagem ao usuário de que o produto foi adicionado ao carrinho.
+  alert(`Produto "${productName}" adicionado ao carrinho por R$ ${price}.`);
+}
