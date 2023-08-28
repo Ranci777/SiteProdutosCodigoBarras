a61ae05a9c004366da3526220c7633c28274a854
Vitória <101210358+Ranci777@users.noreply.github.com>
2023-08-28 15:06:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
-function addToCart(productName, price) {
-  // Aqui você pode implementar a lógica para adicionar o produto ao carrinho de compras.
-  // Por exemplo, você pode usar uma API para enviar os dados para um servidor ou apenas
-  // exibir uma mensagem ao usuário de que o produto foi adicionado ao carrinho.
-  alert(`Produto "${productName}" adicionado ao carrinho por R$ ${price}.`);
+// Captura o botão "Adicionar ao Carrinho"
+const addToCartButton = document.querySelectorAll(".product-card button");
+// Inicializa o contador do carrinho
+let cartCount = 0;
+// Adiciona um evento de clique a todos os botões "Adicionar ao Carrinho"
+addToCartButton.forEach((button) => {
+  button.addEventListener("click", function () {
+    const product = this.parentElement; // O elemento pai do botão é a div .product-card
+
+    // Extrai informações do produto
+    const productName = product.querySelector("h2").textContent;
+    const price = parseFloat(
+      product.querySelector(".price").textContent.replace("R$", "")
+    ); // Converte o preço para float
+    const quantity = parseInt(product.querySelector("#quantity").value);
+
+    // Chama a função para adicionar o item ao carrinho
+    addToCart(quantity, productName, price);
+
+    // Incrementa o contador do carrinho
+    cartCount += quantity;
+    updateCartCount();
+  });
+});
+
+// Função para atualizar o contador do carrinho
+function updateCartCount() {
+  const cartCountElement = document.getElementById("cartcount");
+  cartCountElement.textContent = cartCount;
 }
-dispatchEvent(new Event("load"));
-function gerarCodigoDeBarras() {
-  var codigo = document.getElementById("codigo").value;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      document.getElementById("barcodeImage").src = xmlhttp.responseText;
-    }
-    xmlhttp.open("POST", "codigoDeBarras.php", true);
-    xmlhttp.setRequestHeader(
-      "Content-type",
-      "application/x-www-form-urlencoded"
-    );
-    xmlhttp.send("codigo=" + codigo);
-    var codigo = document.getElementById("codigo").value;
-    // Lógica para gerar a imagem do código de barras (utilizando a biblioteca GD neste exemplo)
-    var image = new Image();
-    image.onload = function () {
-      document.getElementById("barcodeImage").src = image.src;
-      image.src = "gerarCodigoDeBarras.php?codigo=" + codigo;
-    };
-  };
-}
-function enviado(numberProduct, codigoDeBarras) {
-  // Aqui você pode implementar a lógica para adicionar o produto ao carrinho de compras.
-  // Por exemplo, você pode usar uma API para enviar os dados para um servidor ou apenas
-  // exibir uma mensagem ao usuário de que o produto foi adicionado ao carrinho.
-  alert(
-    `Produto "${numberProduct}" adicionado ao carrinho por R$ ${codigoDeBarras}.`
-  );
+
+// Função para adicionar um item ao carrinho
+function addToCart(quantity, productName, price) {
+  alert("Produto adicionado ao carrinho.");
+  // Cria o elemento do item do carrinho
+  const cartItem = document.getElementById("cart").content.cloneNode(true);
+  cartItem.classList.add("cart");
+  cartItem.innerHTML = `
+    <img src="img/${productName}.jpg" alt="${productName}" />
+    <div class="cart-item-info">
+      <h4>${productName}</h4>
+      <h5>R$ ${price.toFixed(2)}</h5>
+      <span class="remove-item">Remover</span>
+    </div>
+    <div class="cart-item-quantity">
+      <i class="fas fa-chevron-up"></i>
+      <p class="item-amount">${quantity}</p>
+      <i class="fas fa-chevron-down"></i>
+    </div>
+  `;
+  // Adiciona o elemento do item do carrinho ao carrinho
+  const cart = document.getElementById("cart");
+  cart.appendChild(cartItem);
 }
